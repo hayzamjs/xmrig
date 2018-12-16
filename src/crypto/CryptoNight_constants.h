6e21f095b4b92a577a3f563f514b315d631c56ae
DR HOUSE MD <hayzam@gmail.com>
2018-12-16 21:37:36 +0530

    CN/XTL_V2 changes

--- src/crypto/CryptoNight_constants.h
+++ src/crypto/CryptoNight_constants.h
@@ -37,7 +37,7 @@ namespace xmrig
 
 constexpr const size_t   CRYPTONIGHT_MEMORY       = 2 * 1024 * 1024;
 constexpr const uint32_t CRYPTONIGHT_MASK         = 0x1FFFF0;
-constexpr const uint32_t CRYPTONIGHT_ITER         = 0x80000;
+constexpr const uint32_t CRYPTONIGHT_ITER         = 0x40000;
 constexpr const uint32_t CRYPTONIGHT_MSR_ITER     = 0x40000;
 constexpr const uint32_t CRYPTONIGHT_XAO_ITER     = 0x100000;
 
